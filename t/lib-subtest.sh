6e887d4817b6206690a8887fcfc2787c5b90b609
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-06-14 12:48:08 +0200

    test-lib tests: split up "write and run" into two functions
    Refactor the function to write and run tests of the test-lib.sh output
    into two functions.
    
    When this was added back in 565b6fa87bb (tests: refactor mechanics of
    testing in a sub test-lib, 2012-12-16) there was no reason to do this,
    but since we started supporting test arguments in
    517cd55fd51 (test-lib: self-test that --verbose works, 2013-06-23)
    we've started to write out duplicate tests simply to test different
    arguments, now we'll be able to re-use them.
    
    This change doesn't consolidate any of those tests yet, it just makes
    it possible to do so. All the changes in t0000-basic.sh are a simple
    search-replacement.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/lib-subtest.sh
+++ t/lib-subtest.sh
@@ -1,13 +1,7 @@
-_run_sub_test_lib_test_common () {
-	neg="$1" name="$2" descr="$3" # stdin is the body of the test code
-	shift 3
+write_sub_test_lib_test () {
+	name="$1" descr="$2" # stdin is the body of the test code
 	mkdir "$name" &&
 	(
-		# Pretend we're not running under a test harness, whether we
-		# are or not. The test-lib output depends on the setting of
-		# this variable, so we need a stable setting under which to run
-		# the sub-test.
-		sane_unset HARNESS_ACTIVE &&
 		cd "$name" &&
 		write_script "$name.sh" "$TEST_SHELL_PATH" <<-EOF &&
 		test_description='$descr (run in sub test-lib)
@@ -19,7 +13,22 @@ _run_sub_test_lib_test_common () {
 		# Point to the t/test-lib.sh, which isn't in ../ as usual
 		. "\$TEST_DIRECTORY"/test-lib.sh
 		EOF
-		cat >>"$name.sh" &&
+		cat >>"$name.sh"
+	)
+}
+
+_run_sub_test_lib_test_common () {
+	neg="$1" name="$2" descr="$3" # stdin is the body of the test code
+	shift 3
+	(
+		cd "$name" &&
+
+		# Pretend we're not running under a test harness, whether we
+		# are or not. The test-lib output depends on the setting of
+		# this variable, so we need a stable setting under which to run
+		# the sub-test.
+		sane_unset HARNESS_ACTIVE &&
+
 		export TEST_DIRECTORY &&
 		TEST_OUTPUT_DIRECTORY=$(pwd) &&
 		export TEST_OUTPUT_DIRECTORY &&
@@ -31,6 +40,18 @@ _run_sub_test_lib_test_common () {
 			! ./"$name.sh" "$@" >out 2>err
 		fi
 	)
+}
+
+write_and_run_sub_test_lib_test () {
+	name="$1" descr="$2" # stdin is the body of the test code
+	write_sub_test_lib_test "$@" || return 1
+	_run_sub_test_lib_test_common '' "$@"
+}
+
+write_and_run_sub_test_lib_test_err () {
+	name="$1" descr="$2" # stdin is the body of the test code
+	write_sub_test_lib_test "$@" || return 1
+	_run_sub_test_lib_test_common '!' "$@"
 }
 
 run_sub_test_lib_test () {
