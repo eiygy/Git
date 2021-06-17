b05d9909673fd99de9209530e88013d19696862a
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-06-17 12:22:41 +0200

    git hook run: add an --ignore-missing flag
    For certain one-shot hooks we'd like to optimistically run them, and
    not complain if they don't exist. This will be used by send-email in a
    subsequent commit.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/hook.c
+++ builtin/hook.c
@@ -22,10 +22,13 @@ static int run(int argc, const char **argv, const char *prefix)
 	int i;
 	struct run_hooks_opt opt = RUN_HOOKS_OPT_INIT;
 	int rc = 0;
+	int ignore_missing = 0;
 	const char *hook_name;
 	const char *hook_path;
 
 	struct option run_options[] = {
+		OPT_BOOL(0, "ignore-missing", &ignore_missing,
+			 N_("exit quietly with a zero exit code if the requested hook cannot be found")),
 		OPT_END(),
 	};
 
@@ -49,11 +52,14 @@ static int run(int argc, const char **argv, const char *prefix)
 	/*
 	 * We are not using run_hooks() because we'd like to detect
 	 * missing hooks. Let's find it ourselves and call
-	 * run_found_hooks() instead.
+	 * run_found_hooks() instead...
 	 */
 	hook_name = argv[0];
 	hook_path = find_hook(hook_name);
 	if (!hook_path) {
+		/* ... act like run_hooks() under --ignore-missing */
+		if (ignore_missing)
+			return 0;
 		error("cannot find a hook named %s", hook_name);
 		return 1;
 	}
